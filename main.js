1b79696ded67292a1c81741a5c997d4925e148ed
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2021-04-10 23:13:21 -0300

    Treinando codigos JS

---
+++ main.js
@@ -0,0 +1,69 @@
+/*var nome = "Everton Oliveira";
+var idade = 38;
+var idade2 = 10;
+var frase = "Japão é o maior time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão","Brasil"));
+
+console.log(frase.toLowerCase())
+
+var n1 = 5;
+var n2 = 3;
+
+console.log(n1*n2);
+
+
+var lista = ["maça","pera","laranja"];
+lista.push("uva");
+//lista.pop();
+//lista.reverse();
+//lista.toString();
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+console.log(lista.join(" | "));
+console.log(lista.join(" "));
+
+//console.log(lista)
+
+var fruta = {nome: "maça",cor:"vermelha"};
+var fruta = [{nome: "maça",cor:"vermelha"}, {nome: "uva",cor:"roxa"}];
+console.log(fruta);
+
+var idade = prompt("Qual sua idade");
+if(idade > 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+
+var count = 0;
+while(count <= 5){
+    alert(count);
+    count++;
+};
+
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+
+var d = new Date();
+
+alert(d.getMonth()+1);
+alert(d.getHours());
+alert(d.getMinutes());*/
+
+
+/*
+var lista = [nome: “rosa” , “cravo” , “tulipa”, “margarida”];
+console.log(lista[2]);
+*/
+
+Alert ("Digital Innovation One:" + formacoes);
